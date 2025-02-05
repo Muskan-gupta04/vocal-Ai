805264cf083c4b54ea4f1a9b250d6b3324a07ffe
Muskan-gupta04 <mg3285632@gmail.com>
2025-02-05 13:48:32 +0530

    commited  vocal-ai app

---
+++ VocalizeAI/main.py
@@ -0,0 +1,16 @@
+import pyttsx3
+
+def text_to_speech(text):
+    engine = pyttsx3.init()
+    
+    engine.say(text)
+    engine.runAndWait()
+
+if __name__ == '__main__':
+    print("Welcome to VocalizeAI!")
+    while True:
+        text = input("Enter whatever you want me to speak (type 'exit' to stop): ")
+        
+        if text.lower() == "exit":
+            break
+        text_to_speech(text)
